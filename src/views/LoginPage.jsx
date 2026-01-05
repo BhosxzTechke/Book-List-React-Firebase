fc5df4046165cf2fcb1e412e353f2bacedbafea5
BhosxzTechke <danmichaelantiquina9@gmail.com>
2026-01-05 15:43:06 +0800

    Login title changes

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -102,7 +102,7 @@ function LoginPage() {
         
         <div className="container login-page">
           <section>
-            <h1>Welcome to the Book App</h1>
+            <h1>Welcome to the bossKe Books Library</h1>
             <p>Login or create an account to continue</p>
             <div className="login-type">
               <button 
